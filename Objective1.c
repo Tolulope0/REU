25a1478bfd2d018117d1af28df496bbf80a35b41
Tolulope0 <44985202+Tolulope0@users.noreply.github.com>
2019-01-23 11:40:45 -0600

    fixing
    debug

--- Objective1.c
+++ Objective1.c
@@ -15,46 +15,52 @@ int main(int argc, char*argv[])
 	char line[100];
 	fp=fopen("request.txt","r");
 	//2D MATRIX 
-	int **Matrix=malloc(sizeof(int*)*2);
-	Matrix[0]=malloc(sizeof(int)*SIZE);
-	Matrix[1]=malloc(sizeof(int)*SIZE);
+	int **Matrix=malloc(sizeof(int*)*2);//2
+	Matrix[0]=malloc(sizeof(int)*SIZE);//1
+	Matrix[1]=malloc(sizeof(int)*SIZE);//1
+	//Matrix=4
 	//2D MATRIX CREATED
 	//i created a set number of traces. I am currently thinking of a way to let the number not be set.
 	int **R;
 	int timestamps=1;
 	int j=0;
 	int go=0;
-	int numR=0;
+	int numR=4;
 	while (fgets(line, 100, fp)!=NULL)
 	{
-		//	printf("%s",line);
+			printf("%s",line);
 		if(j==0)
 		{
 			token= strtok(line,"\n");
-			numR=atoi(token);//number of R in file
+			//numR=atoi(token);//number of R in file
 			R=malloc(sizeof(int*)*numR);
-
+			len=malloc(sizeof(int)*numR);
 			j++;
 
 		}
 		else
-			token= strtok(NULL,":");
-			int numNR=atoi(token);//number of items in each R
+		{		token= strtok(NULL,":");
+			int numNR=10;
+		//	int numNR=atoi(token);//number of items in each R
 			int l;
-			len=malloc(sizeof(int)*numNR);
+			len[go]=numNR;
 			R[go]=malloc(sizeof(int)*numNR);
-			for(l=0;l<numNR;l++)
+			/*for(l=0;l<numNR-1;l++)
 			{
 				token= strtok(NULL,",");
 				R[go][l]=atoi(token);
 			}
-			go++;
+				token= strtok(NULL,"\n");
+				R[go][l+1]=atoi(token);
 
+			*/
+			go++;
+		}
 	}
 	//int **Requests=malloc(sizeof(int*)*j);
 	fclose(fp);
 	j=0;
-
+/*
 	for(timestamps=1;timestamps<SIZE+1;timestamps++)
 	{
 		srand(time(NULL));
@@ -68,20 +74,17 @@ int main(int argc, char*argv[])
 		Matrix[0][0]=hold;
 		Matrix[1][0]=timestamps;
 	}
-
+*/
 	int k=0;
 	while(k<numR)
 	{
-	free(R[k]);
-	k++;
+		free(R[k]);
+		k++;
 	}
 	free(R);
 	k=0;
-	while(k<SIZE)
-	{
 	free(Matrix[0]);
 	free(Matrix[1]);
-	}
 	free(Matrix);
 	free(len);
 
