f913e894e79127ac7006dc513ef54a48516e0a07
Tolulope0 <44985202+Tolulope0@users.noreply.github.com>
2019-03-21 20:57:25 -0500

    Debugging
    Modified Ob1 c++
    and Ob2MD c++
    and new trace doc

--- Objective1.cpp
+++ Objective1.cpp
@@ -8,73 +8,64 @@ using namespace std;
 
 class P
 {
-	public:
-		vector<int> sequence;// range of process
-		int takeRequest;// the amount to take from each process
-		int max;
-		int min;
-		int Distribution;// how the interleaved pattern num of request are take for process
-		//Ended up not using order
-		int order;//this is for the interleaving pattern; the order that each group is taken. ex:P[0]-P[1]-P[2] or P[1]-P[0]-P[2] or P[2]-P[1]-P[0] etc..
-		//in this case 1= sequencial, 2 equals random
-		int minP,maxP;
-		
-		int pattern;//interleaving pattern- randomly generate the # of request taken from each process  based on the # of request in each process, and store it here
-		
+public:
+	vector<int> sequence;// range of process
+	int takeRequest;// the amount to take from each process
+	int max;
+	int min;
+	int Distribution;// how the interleaved pattern num of request are take for process
+	//Ended up not using order
+	int order;//this is for the interleaving pattern; the order that each group is taken. ex:P[0]-P[1]-P[2] or P[1]-P[0]-P[2] or P[2]-P[1]-P[0] etc..
+	//in this case 1= sequencial, 2 equals random
+	int minP, maxP, start;
+	int pattern;//interleaving pattern- randomly generate the # of request taken from each process  based on the # of request in each process, and store it here
+
 };
 
 //things that need to be done
 //there is a bug in this function find it
 ////because it is causing done to come out as 109 instead of 100
-vector <int> take (vector<int> s , P pro)//this function will deal with distribution of and interleaving
+vector <int> take(vector<int> s, P pro)//this function will deal with distribution of and interleaving
 {
-	int u=0;
-	int ran=0;
-	while(ran<pro.sequence.size())
+	int ran = pro.start;
+	int u = 0;
+	if(ran>pro.sequence.size())
+		ran=0;	
+	while (ran < pro.sequence.size())
 	{
-		if(pro.takeRequest < pro.pattern)
+
+	//	if (pro.takeRequest < pro.pattern)
+	//	{
+	//			s.push_back(pro.sequence[ran]);// range of process
+	//			u++;
+	//	}
+	//	else
 		{
-
-			if(pro.sequence[ran]!=-1)
-
-			{	
-				s.push_back(pro.sequence[ran]);// range of process
-					pro.sequence[ran]=-1;
-				u++;	
-			}
-		}
-		else
-		{	
-			if(pro.Distribution==1)//seqeun
+			if (pro.Distribution == 1)//seqeun
 			{
-				
-				if(pro.sequence[ran]!=-1)
-				{
 					s.push_back(pro.sequence[ran]);
 					u++;//checks to see if all require request where taken
-					pro.sequence[ran]=-1;
-				}
+	//		cout<<"KKKKLL  u= "<<u<<"ran= "<<ran<<"SIZE"<<pro.sequence.size()<<endl;
 			}
 			else//random
 			{
-				int random=rand()%pro.sequence.size();// getting number for interleaving pattern for each process
-			//	cout<<"RAN: "<<random<<" Range size = "<<pro.range.size()<<endl;
-                        	        
-					if(pro.sequence[ran]!=-1)
-					{	
-						s.push_back(pro.sequence[random]);
-                        	        	u++;//checks to see if all require request where taken
-					}
-
-			}	
+	//		cout<<"UUUUUUUUKKKKLL"<<endl;
+				int random = rand() % pro.sequence.size();// getting number for interleaving pattern for each process
+					s.push_back(pro.sequence[random]);
+					u++;
+			}
 		}
 		ran++;
-		if(u==pro.pattern)
+		if(ran==pro.sequence.size())
 		{
-	//		cout<<"u="<<u<<endl;
+			ran=0;	
+		}
+		if (u == pro.pattern)
+		{
+	//				cout<<"u= "<<u<<endl;
 			break;
 		}
-		
+
 	}
 	return s;
 }
@@ -86,173 +77,174 @@ int main(int argc, char*argv[])
 	vector <P> processes;//double vector created
 	ifstream infile;
 	infile.open("input.txt");
-	if(!infile.is_open())
+	if (!infile.is_open())
 	{
-		cout<<"file not found"<<endl;
+		cout << "file not found" << endl;
 	}
 	else
 	{
-		int taketotal=0;//will contain the total number of objects taken from all processes
+		int taketotal = 0;//will contain the total number of objects taken from all processes
 		char line[1000];
-		infile>> line;
-		//cout<<line<<endl;
-		//char hold [1000]=line;
-		char *token ;
-		//token = strtok(line,",");
-		//line =hold;
-		int numOfProcesses=0;
-		//= atoi(token);//number of processes in file
-		//token = strtok(NULL,",");
-		//int PatternType=0;
-		//	atoi(token);
-		int loop=0;
+		infile >> line;
+		char *token;
+		int numOfProcesses = 0;
+		int loop = 0;
 		P pro;
 		//while(loop<numOfProcesses)
 		while (!infile.eof())
 		{
 			numOfProcesses++;
-			token = strtok(line,",");
-			int min=atoi(token);
-			token = strtok(NULL,",");
-			int max=atoi(token);
-			int random=(rand()%max+min)%10;// getting number for interleaving pattern for each process
-			pro.pattern=random;	
-			pro.max=max;	
-			pro.min=min;	
-			while (min<=max)
+			token = strtok(line, ",");
+			int min = atoi(token);
+			//cout<<"Min"<<min<<endl;
+			token = strtok(NULL, ",");
+			int max = atoi(token);
+			//cout<<"Max"<<max<<endl;
+			//token = strtok(NULL,",");
+			//pro.start=atoi(token);
+			int random = (rand() % max + min) % 10;// getting number for interleaving pattern for each process
+			pro.pattern = random;
+			//cout<<"Pattern"<<random<<endl;
+			pro.max = max;
+			pro.min = min;
+			while (min <= max)
 			{
 				pro.sequence.push_back(min);
 				min++;
 			}
-			token = strtok(NULL,",");
-			taketotal+=atoi(token);
-			pro.takeRequest=atoi(token);
-			token = strtok(NULL,",");
-			pro.Distribution=atoi(token);
-			token = strtok(NULL,",");
-			pro.minP=atoi(token);
-			token = strtok(NULL,",");
-			pro.maxP=atoi(token);
-			token = strtok(NULL,"\n");
-			pro.order=atoi(token);
-			//cout<<token<<endl;
+			token = strtok(NULL, ",");
+			//cout<<"taketotal"<<token<<endl;
+			pro.takeRequest = atoi(token);
+			taketotal += atoi(token);
+			token = strtok(NULL, ",");
+			//cout << "start= " << token << endl;
+			pro.start = atoi(token);
+			//token = strtok(NULL,",");
+			token = strtok(NULL, ",");
+			//cout<<"Distribution"<<token<<endl;
+			pro.Distribution = atoi(token);
+			token = strtok(NULL, ",");
+			//cout<<"MinP"<<token<<endl;
+			pro.minP = atoi(token);
+			token = strtok(NULL, ",");
+			//cout<<"MaxP"<<token<<endl;
+			pro.maxP = atoi(token);
+			token = strtok(NULL, "\n");
+			//cout<<"Order"<<token<<endl;
+			pro.order = atoi(token);
 			processes.push_back(pro);
 			loop++;
 			pro.sequence.clear();
-			infile>>line;
-	//	cout<<"min= "<<pro.min<<" max= "<<max<<"Distribution:"<<pro.Distribution<<"Request to take: "<<pro.takeRequest<<"Pattern range: ["<<pro.minP<<":"<<pro.maxP<<"]"<<" Pattern order: "<<pro.order<<endl;
-		}
-		
-		infile.close();
-		
-		
+			infile >> line;
 
-		int done =0;
-       		//	numOfProcesses
-		//cout<<"taketotal= "<<taketotal<<endl;
-		int i =0;
+			//cout<<"min= "<<pro.minP<<" max= "<<pro.maxP<<"Distribution:"<<pro.Distribution<<"Request to take: "<<pro.takeRequest<<"Pattern range: ["<<pro.minP<<":"<<pro.maxP<<"]"<<" Pattern order: "<<pro.order<<endl;
+		}
+
+		infile.close();
+		int done = 0;
+		int i = 0;
 		srand(time(NULL));
 		vector<int>  ran;
 		vector <int>::iterator it;
-		while (ran.size()<numOfProcesses)//forcing processes to run in sequence
+		while (ran.size() < numOfProcesses)//forcing processes to run in sequence
 		{
-			int rando =0;
+			int rando = 0;
 			ran.push_back(rando);
 			rando++;
-					//		cout<<"random order"<<rando<<"ran.size()"<<ran.size()<<endl;
 		}
-
-
-			
-		int gp=0;
-//		vector<int>seq;	
+		int gp = 0;
 		vector <int> seq;//1d integer vector
-
-		vector<int>hol;	
-		int opi=0;
-		int op=0;
-                int io=0;
-	//	while ((done<taketotal)&&(op!=20)) //USED FOR TESTING
-		while (done<taketotal)
+	//	cout<<"TakeTotal: "<<taketotal<<endl;
+		vector<int>hol;
+		int opi = 0;
+		int op = 0;
+		int io = 0;
+	//		while ((done<taketotal)&&(op!=20)) //USED FOR TESTING
+		while (done < taketotal)
 		{
-			 if(i == numOfProcesses)
-                        {
-                                i=0;
-                                opi++;
-                        }
-
-			 
-                	if(opi==0)
+			if (i == numOfProcesses)
 			{
-        			int go=processes[i].minP;
-				while(go<processes[i].maxP)
-				{
-                			hol.push_back(go);
-                			go++;
-		
-				}
-
+				i = 0;
 			}
-      			if(processes[i].order == 1)
-        		{
-				if(gp==hol.size())
+			if (opi < numOfProcesses)
+			{
+				int go = processes[i].minP;
+				processes[i].start = (find(processes[i].sequence.begin(),processes[i].sequence.end(),processes[i].start)-processes[i].sequence.begin());
+				while (go < processes[i].maxP)
 				{
-					gp=0;
+					hol.push_back(go);
+					go++;
 				}
-                        
-              			processes[i].pattern=hol[gp];
+				opi++;
+			}
+			if (processes[i].order == 1)
+			{
+				if (gp == hol.size())
+				{
+					gp = 0;
+				}
+				processes[i].pattern = hol[gp];
 				gp++;
-
-        		}
-        		if(processes[i].order == 2)
-        		{
-				processes[i].pattern=rand()%processes[i].maxP;// getting number for interleaving pattern for each process	
+			//cout<<"Pattern of "<<"P"<<i<<" "<<processes[i].pattern<<endl;
 			}
-			if (processes[i].takeRequest<processes[i].pattern)
-                 	{
-          //                	        cout<<"Request :"<<processes[i].takeRequest<<"  pattern= "<<processes[i].pattern<<endl;
-				if(processes[i].takeRequest==0)
-					io=9;
-				done+=processes[i].takeRequest;
-                                processes[i].pattern=processes[i].takeRequest;
-				processes[i].takeRequest=0;
-				if(io!=9)//if request are zero than dont call function
-					seq=take(seq,processes[i]);
-	//	cout<<"seq= "<<seq.size()<<endl;
-                        }	
+			if (processes[i].order == 2)
+			{
+				processes[i].pattern = rand() % processes[i].maxP;// getting number for interleaving pattern for each process	
+			//cout<<"Pattern of "<<"P"<<i<<" "<<processes[i].pattern<<endl;
+			}
+			if (processes[i].takeRequest < processes[i].pattern||((processes[i].takeRequest==0)&&(processes[i].pattern==0)))
+			{
+	//			cout << "Request :" << processes[i].takeRequest << "  pattern= " << processes[i].pattern << endl;
+				if (processes[i].takeRequest == 0)
+					io = 9;
+				done += processes[i].takeRequest;
+				processes[i].pattern = processes[i].takeRequest;
+				processes[i].start += processes[i].pattern;
+				//cout << "START = " << processes[i].start << endl;
+
+				if (processes[i].start > processes[i].max)
+					processes[i].start = processes[i].min;
+				processes[i].takeRequest = 0;
+				if (io != 9)//if request are zero than dont call function
+					seq = take(seq, processes[i]);
+	//				cout<<"seq= "<<seq.size()<<endl;
+			}
 			else
 			{
-        //                  	        cout<<"Request :"<<processes[i].takeRequest<<"  pattern= "<<processes[i].pattern<<endl;
-					//cout<<"Done ="<<done<<endl;
-				done+=processes[i].pattern;
-				seq=take(seq,processes[i]);
-		
-				processes[i].takeRequest-=processes[i].pattern;
-	//	cout<<"seq= "<<seq.size()<<endl;
+	//			cout << "Request :" << processes[i].takeRequest << "  pattern= " << processes[i].pattern << endl;
+				//				cout<<"Done ="<<done<<endl;
+				done += processes[i].pattern;
+				//cout << "START = " << processes[i].start << endl;
+				if (processes[i].start > processes[i].max)
+					processes[i].start = processes[i].min;
+				if(processes[i].pattern!=0)
+				{
+					seq = take(seq, processes[i]);
+	//			cout<<"BLUEBERRY PIE"<<endl;
+				}//processes[i].start+=processes[i].takeRequest;
+				processes[i].start += processes[i].pattern;
+
+				processes[i].takeRequest -= processes[i].pattern;
+	//		cout<<"1seq= "<<seq.size()<<endl;
 			}
+			io = 0;
+	//		cout<<"Ddone= "<<done<<endl;
+			op++;
+			i++;
+		}
+	//		cout<<"Seq= "<<seq.size()<<endl;
 
-		
-		io=0;
-	//		cout<<"done= "<<done<<endl;
-		op++;	
-		i++;
+		int g = 0;
+		ofstream outFile;
+		outFile.open("trace-SRS-SRS.txt");
+		while (g < seq.size())
+		{
+			outFile<<seq[g]<<"\n";
+			//cout << seq[g] << "\n";
+			g++;
+		}
+		outFile.close();
 	}
-	//	cout<<"seq= "<<seq.size()<<endl;
-	int g=0;
-       ofstream outFile;
-       outFile.open("trace-SRS-SRS.txt");
-       	while(g<seq.size())
-        {
-               outFile<<seq[g]<<"\n";
-               	//cout<<seq[g]<<"\n";
-                g++;
-        }
-	outFile.close();
-	
-	
-	
-	}
-
 }
 
 
--- Objective2-MOD.cpp
+++ Objective2-MOD.cpp
@@ -2,6 +2,8 @@
 #include<fstream>
 #include<vector>
 #include<cstring>
+#include<algorithm>
+#include<bits/stdc++.h>
 using namespace std;
 class Buff
 {
@@ -15,14 +17,58 @@ class Buff
 	}
 };
 
+int miss=0, hit=0;
+bool check=false;
+class LRU
+{
+	list<int>buf;
+	unordered_map<int,list<int>::iterator>my;
+	int csize;
+
+	public:
+	LRU(int n)
+	{
+		csize=n;
+	}	
+	void refer(int x)
+	{
+		if(my.find(x) == my.end())
+		{
+			miss++;
+			if(buf.size() ==csize)
+			{
+				int last=buf.back();
+				buf.pop_back();
+				my.erase(last);
+				
+
+
+			//	cout<<"MISS"<<endl;
+			}
+
+	
+		}
+		else
+		{
+			buf.erase(my[x]);
+			hit++;
+
+			check=true;
+
+		}
+		buf.push_front(x);
+		my[x]=buf.begin();
+	}
+
+};
 
 int main()
 {
-        
+        LRU BUFFER(5);
 	ifstream infile;
 	char *token;	
 	char line [10];
-       	infile.open("trace-SRS-SRS.txt");
+       	infile.open("trace-SSS-SRS.txt");
         if(!infile.is_open())
         {
                 cout<<"file not found"<<endl;
@@ -36,60 +82,101 @@ int main()
         	sequence.push_back(atoi(token));
         	while(!infile.eof())
         	{	infile>>line;
-        	   	//cout<<"token = "<<token<<endl;
+        	   	cout<<"token = "<<token<<endl;
 		       	token=strtok(line,"\n");
         	        sequence.push_back(atoi(token));
         	}
 		sequence.pop_back();
-		//cout<<sequence.size();
-        	vector <Buff> Prebuff;
-		vector <int >bufferb;
+		//cout<<"S Size = "<<sequence.size();
+        	vector <int> Prebuff;
+		//vector <int >bufferb;
 		int h=0;
         	int counter=0;
-        	bool check=false;
+
 		int  cat=0;
-        	int miss=0, hit=0;
+			bool PreOn=false;
+
 		vector <int>::iterator it;
-        	while (counter <100)
+        	/*
+		while (counter <100)
+		{
+			int see=sequence[counter];
+                	cout<<see<<endl;
+			counter++;
+		}
+		*/
+		while (counter <100)
         	{        
                 	check=false;
 			int see=sequence[counter];
                 	cout<<see<<endl;
 
-			if(cat==0)
+			if(cat==0)//beginning of sequence
 			{
-                		cout<<"HI"<<endl;
-				bufferb.push_back(see);
+  //              		cout<<"START"<<endl;
+				Prebuff.push_back(sequence[counter]);
+//				cout<<"Prebuff "<<Prebuff[Prebuff.size()-1]<<endl;
 				cat=100;
 			}
-			if(Prebuff.size()!=0)
-			{
-			
-				while(h<Prebuff.size())
-                        	{
-                                	cout<<"seq: "<<sequence[counter]<<" Prebuff: "<<Prebuff[h].value<<endl;
-					if(sequence[counter]== Prebuff[h].value)
+			else
+			{	
+				if(PreOn==true)
+					BUFFER.refer((Prebuff[h]));
+				int g=	Prebuff[Prebuff.size()-1];
+				g++;
+//				cout<<"sequence[counter]= "<<sequence[counter]<<" Prebuff+1 ="<<g<<endl;
+				
+				if(sequence[counter]==((Prebuff[(Prebuff.size())-1])+1))
+				{
+//				
+               // 			cout<<"CONTINUE"<<endl;
+					Prebuff.push_back(sequence[counter]);
+				
+				}
+				else
+				{	
+					Prebuff.clear();
+			//		Prebuff.push_back(sequence[counter]);
+					counter--;
+//					cout<<"CLEAR"<<endl;
+					cat=0;
+				}
+				if((Prebuff.size()==5))
+				{
+//                                	cout<<"Long enough sequence!"<<endl;
+			 PreOn=true;
+				
+					while(h<Prebuff.size())
+                        		{
+
+						BUFFER.refer((Prebuff[h]+1));
+						BUFFER.refer((Prebuff[h]+2));
+					//Prebuff.push_back(Buff(see+2));	//start prefetching for this sequence
+					//Prebuff.push_back(Buff(see+3));	//start prefetching for this sequence
+					/*if(sequence[counter]== Prebuff[h])
                                 	{
                                 	     	Prebuff[h].used++;
 					     	hit++;
                                 	        check =true;
-                                	}
-                                	h++;
-                        	}
-			h=0;
-			}	
-				cout<<"buffer = "<<bufferb[bufferb.size()-1]+1<<endl;
-			if(see==bufferb[bufferb.size()-1]+1)
+                                	}*/
+                                		h++;
+                        		}
+			
+					h=0;
+				}	
+				else if(PreOn=false)
+				{
+				miss++;
+				}
+			//	cout<<"buffer = "<<bufferb[bufferb.size()-1]+1<<endl;
+		/*	if(see==bufferb[bufferb.size()-1]+1)
 			{	
 				bufferb.push_back(see);
 				cout<<"see = "<<see<<endl;
 			}
 			else
 			{		if(bufferb.size()==5)
-				{
-					Prebuff.push_back(Buff(see+1));	//start prefetching for this sequence
-					Prebuff.push_back(Buff(see+2));	//start prefetching for this sequence
-					Prebuff.push_back(Buff(see+3));	//start prefetching for this sequence
+
 				}
 				else
 				{	
@@ -97,19 +184,22 @@ int main()
 				cat=0;
 				cout<<"see =n C"<<cat<<endl;
 				}
-		/*	bufferb.push_back(see-1);
+	*/	/*	bufferb.push_back(see-1);
                 	bufferb.push_back(see+50);	
                 */
+	//		}
 			}
-			counter++;
-        	//	bufferb.clear();
-        		if(check==false)
-        		{
-        			miss++;
-        		}
 
+        	//	bufferb.clear();
+	       	//	if(check==false)
+        	//	{
+        		//	miss++;
+        	//	}
+			//else
+			//	hit++;
+			counter++;
 //LRU
-			if(Prebuff.size()==10)//size of prefetch block is 10
+	/*		if(Prebuff.size()==10)//size of prefetch block is 10
 			{
 				int fo=0;int hold;
 				while(fo<9)
@@ -123,8 +213,10 @@ int main()
 				Prebuff.erase(Prebuff.begin()+hold);
 	
 			}	
-        	}
-       		cout<<"Total number of hits: "<<hit<<" Total number of misses: "<<miss<<endl;
+        	*/
+		}
+       	
+		cout<<"Total number of hits: "<<hit<<" Total number of misses: "<<miss<<endl;
 	}
 
 }
